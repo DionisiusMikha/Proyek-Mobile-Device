efbb6593db269858b8c8e413dc174f3a2ac9ffc7
PokemonApiAir <103976468+PokemonApiAir@users.noreply.github.com>
2024-05-16 12:47:47 +0700

    Splash Tampilan Awal

---
+++ Menghitung_Mimpi/app/src/main/java/id/ac/istts/menghitung_mimpi/layout/SplashActivity.kt
@@ -0,0 +1,22 @@
+package id.ac.istts.menghitung_mimpi.layout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import id.ac.istts.menghitung_mimpi.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Pindah ke MainActivity
+            val intent = Intent(this, LoginRegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
