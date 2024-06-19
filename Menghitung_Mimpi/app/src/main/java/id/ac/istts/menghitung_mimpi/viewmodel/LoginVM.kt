d25bb2519d7299bce880eed0c19984b86524bacd
PokemonApiAir <103976468+PokemonApiAir@users.noreply.github.com>
2024-06-19 14:04:10 +0700

    Ya

--- Menghitung_Mimpi/app/src/main/java/id/ac/istts/menghitung_mimpi/viewmodel/LoginVM.kt
+++ Menghitung_Mimpi/app/src/main/java/id/ac/istts/menghitung_mimpi/viewmodel/LoginVM.kt
@@ -1,23 +1,28 @@
 package id.ac.istts.menghitung_mimpi.viewmodel
 
+import id.ac.istts.menghitung_mimpi.viewmodel.API.Interface.LoginService
 import id.ac.istts.menghitung_mimpi.viewmodel.API.Repository.ApiException
 import id.ac.istts.menghitung_mimpi.viewmodel.API.Repository.LoginRepo
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.ViewModel
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.Dispatchers
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class LoginVM(private val loginRepo: LoginRepo): ViewModel() {
-//    val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-//    private val retrofit = Retrofit.Builder()
-//        .baseUrl("http://10.0.2.2:3000/api/")
-//        .addConverterFactory(MoshiConverterFactory.create(moshi))
-//        .build()
-//
-//    private val apiService = retrofit.create(LoginService::class.java)
+    val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:3000/api/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
 
-    /*suspend fun login(email: String, password: String, onSuccess: (String) -> Unit, onError: (String) -> Unit) {
+    private val apiService = retrofit.create(LoginService::class.java)
+
+    suspend fun login(email: String, password: String, onSuccess: (String) -> Unit, onError: (String) -> Unit) {
         viewModelScope.launch(Dispatchers.IO) {
             val result = loginRepo.login(email, password)
             println(result)
@@ -31,5 +36,5 @@ class LoginVM(private val loginRepo: LoginRepo): ViewModel() {
                 }
             }
         }
-    }*/
+    }
 }
