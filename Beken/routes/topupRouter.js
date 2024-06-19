a51fbb74d3e4bcda381abb40b46f60a8fcf21789
PokemonApiAir <103976468+PokemonApiAir@users.noreply.github.com>
2024-06-19 13:27:09 +0700

    Hapus Beken

--- Beken/routes/topupRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { topup } = require("../controllers/topupController");
-const { authentication } = require("../controllers/userController");
-
-router.post("/topup", authentication, topup);
-
-module.exports = router;
