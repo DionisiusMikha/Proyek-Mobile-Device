bd70c719b078daab5d45dfe3b51419bd8990f423
Jonathan Wilbert <jonathanwilbert8wd@gmail.com>
2024-06-23 22:51:15 +0700

    update topup

--- Menghitung_Mimpi_API/routes/topupRouter.js
+++ Menghitung_Mimpi_API/routes/topupRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { topup } = require("../controllers/topupController");
+const { topup, changeStatusTopup } = require("../controllers/topupController");
 const { authentication } = require("../controllers/userController");
 
 router.post("/topup", authentication, topup);
+router.put("/updateTopup", authentication, changeStatusTopup);
 
 module.exports = router;
